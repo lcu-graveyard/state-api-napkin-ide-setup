c2565f5b3024a4a6afda1ff8beffdbd81a2afb97
Matt Brooks <matt.brooks@fathym.com>
2020-02-17 14:12:23 -0700

    Adding azure function files

---
+++ GrantAccess.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using LCU.State.API.NapkinIDE.Setup.Models;
+using LCU.State.API.NapkinIDE.Setup.Harness;
+using Microsoft.WindowsAzure.Storage;
+using System.Runtime.Serialization;
+using System.Collections.Generic;
+
+namespace LCU.State.API.NapkinIDE.Setup
+{
+    [DataContract]
+    public class GrantAccessRequest
+    {
+        [DataMember]
+        public virtual string AccessToken { get; set; }
+
+    }
+
+    public static class GrantAccess
+    {
+        [FunctionName("GrantAccess")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Admin, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return await req.Manage<GrantAccessRequest, UserManagementState, UserManagementStateHarness>(log, async (mgr, reqData) =>
+            {
+                log.LogInformation($"Requesting user access...");
+
+                await mgr.GrantAccess(reqData.AccessToken);
+
+                return await mgr.WhenAll(
+                );
+            });
+        }
+    }
+}
